c67585a08c5b8354917186d37687290700a6a0a1
shivu-dg-nuzpapr <shivu.dg@nuzpapr.com>
2021-12-05 15:59:29 +0530

    asdfasdf

--- routes/containers v2/create.ts
+++ routes/containers v2/create.ts
@@ -47,51 +47,51 @@ router.post("/create", async (req: Request, res: Response) => {
     return res.end();
   }
 
-	const result = await docker.run(t, ["-it", "-d", "-p", `${port}:4217`], []).then((data) => {
-		return data;
-	}).catch(err => {
-		return err;
-	})
+	// const result = docker.run(t, ["-it", "-d", "-p", `${port}:4217`], []).then((data) => {
+	// 	return data;
+	// }).catch(err => {
+	// 	return err;
+	// })
 
-	console.log(result);
-	res.end();
+	// console.log(result);
+	// res.end();
 	
-  // docker.createContainer({
-  //   Image: t,
-  //   name: containerName,
-  //   HostConfig: { PortBindings },
-	// 	Cmd: ["-it", "-d"]
-  // }, (err: Error, container) => {
-  //   if (!err) {
-	// 		container?.start(async (err) => {
-	// 			if (!err) {
-	// 				await Container.create({
-	// 					containerId: container.id,
-	// 					baseImage: t,
-	// 					origin: "raw",
-	// 					state: "Running"
-	// 				}).catch(console.error);
-	// 				res.setHeader("Access-Control-Allow-Origin", "*");
-	// 				res.json({
-	// 					ip: process.env.HOST_IP,
-	// 					port
-	// 				}).end();
-	// 			} else {
-	// 				console.error(err);
-	// 				res.status(500)
-	// 				res.json({
-	// 					err: err
-	// 				}).end();
-	// 			}
-	// 		});
-	// 	} else {
-	// 		console.error(err)
-	// 		res.status(500)
-	// 		res.json({
-	// 			err: err
-	// 		}).end();
-	// 	}
-  // })
+  docker.createContainer({
+    Image: t,
+    name: containerName,
+    HostConfig: { PortBindings },
+		Cmd: ["/bin/bash", '-c', 'service shellinabox start']
+  }, (err: Error, container) => {
+    if (!err) {
+			container?.start({}, async (err) => {
+				if (!err) {
+					await Container.create({
+						containerId: container.id,
+						baseImage: t,
+						origin: "raw",
+						state: "Running"
+					}).catch(console.error);
+					res.setHeader("Access-Control-Allow-Origin", "*");
+					res.json({
+						ip: process.env.HOST_IP,
+						port
+					}).end();
+				} else {
+					console.error(err);
+					res.status(500)
+					res.json({
+						err: err
+					}).end();
+				}
+			});
+		} else {
+			console.error(err)
+			res.status(500)
+			res.json({
+				err: err
+			}).end();
+		}
+  })
 
 })
 
