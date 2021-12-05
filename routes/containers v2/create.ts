326253c4e334b2ae7697e7c4ee83558bece7ebd0
shivu-dg-nuzpapr <shivu.dg@nuzpapr.com>
2021-12-05 15:11:23 +0530

    feat: completed container creation

--- routes/containers v2/create.ts
+++ routes/containers v2/create.ts
@@ -12,7 +12,9 @@ router.use(json());
 let port = 3001;
 
 router.post("/create", async (req: Request, res: Response) => {
-	const { containerName, password } = req.body;
+	const { imageName, containerName, password } = req.body;
+  const t = imageName + String(Math.floor(Math.random() * 10 ** 10))
+  
 	port = 3001 + Math.floor(Math.random() * 100);
 
   var PortBindings: Dockerode.PortMap = {
@@ -27,22 +29,60 @@ router.post("/create", async (req: Request, res: Response) => {
     PASSWORD: password
   }
   
-  docker.buildImage({
+  let success =  await docker.buildImage({
     context: path.join(__dirname, "../../lib/Docker"),
     src: ['Dockerfile', 'shellinabox']
-  }, { buildargs }, (error, result) => {
-    if (!error) {
-      console.log(result);
-      res.end();
-    } else {
-      console.log(error);
-      res.status(500);
-      res.json({
-        error
-      });
-      res.end();
-    }
+  }, { buildargs, t }).then(() => {
+    return true;
+  }).catch(err => {
+    // console.log(err);
+    return false;
+  });
+
+  if (!success) {
+    res.status(500);
+    res.json({
+      message: "Failed to while building image."
+    });
+    return res.end();
+  }
+
+  docker.createContainer({
+    Image: t,
+    name: containerName,
+    HostConfig: { PortBindings }
+  }, (err: Error, container) => {
+    if (!err) {
+			container?.start(async (err) => {
+				if (!err) {
+					await Container.create({
+						containerId: container.id,
+						baseImage: t,
+						origin: "raw",
+						state: "Running"
+					}).catch(console.error);
+					res.setHeader("Access-Control-Allow-Origin", "*");
+					res.json({
+						ip: process.env.HOST_IP,
+						port
+					}).end();
+				} else {
+					console.error(err);
+					res.status(500)
+					res.json({
+						err: err
+					}).end();
+				}
+			});
+		} else {
+			console.error(err)
+			res.status(500)
+			res.json({
+				err: err
+			}).end();
+		}
   })
+
 })
 
 export { router as createContainerRouter };
